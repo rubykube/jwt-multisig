edaa9ea563b7735541a5697cee0a8d517281405f
Yaroslav Konoplov <eahome00@gmail.com>
2018-03-24 00:05:03 +0200

    Add docs.

--- lib/jwt-multisig.rb
+++ lib/jwt-multisig.rb
@@ -10,13 +10,64 @@ module JWT
   #
   module Multisig
     class << self
-      def generate_jwt(payload, keychain, algorithms)
+      #
+      # Generates new JWT based on payload, keys, and algorithms.
+      #
+      # @param payload [Hash]
+      # @param private_keychain [Hash]
+      #   The hash which consists of pairs: key ID => private key.
+      #   The key may be presented as string in PEM format or as instance of {OpenSSL::PKey::PKey}.
+      # @param algorithms
+      #   The hash which consists of pairs: key ID => signature algorithm.
+      # @return [Hash]
+      #   The JWT in the format as defined in RFC 7515.
+      #   Example:
+      #     { payload: "eyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzODAsDQogImh0dHA6Ly9leGFtcGxlLmNvbS9pc19yb290Ijp0cnVlfQ",
+      #       signatures: [
+      #         { protected: "eyJhbGciOiJSUzI1NiJ9",
+      #           header: { kid: "2010-12-29" },
+      #           signature: "cC4hiUPoj9Eetdgtv3hF80EGrhuB__dzERat0XF9g2VtQgr9PJbu3XOiZj5RZmh7AAuHIm4Bh-0Qc_lF5YKt_O8W2Fp5jujGbds9uJdbF9CUAr7t1dnZcAcQjbKBYNX4BAynRFdiuB--f_nZLgrnbyTyWzO75vRK5h6xBArLIARNPvkSjtQBMHlb1L07Qe7K0GarZRmB_eSN9383LcOLn6_dO--xi12jzDwusC-eOkHWEsqtFZESc6BfI7noOPqvhJ1phCnvWh6IeYI2w9QOYEUipUTI8np6LbgGY9Fs98rqVt5AXLIhWkWywlVmtVrBp0igcN_IoypGlUPQGe77Rw"
+      #         },
+      #         { protected: "eyJhbGciOiJFUzI1NiJ9",
+      #           header: { kid: "e9bc097a-ce51-4036-9562-d2ade882db0d" },
+      #           signature: "DtEhU3ljbEg8L38VWAfUAqOyKAM6-Xx-F4GawxaepmXFCgfTjDxw5djxLa8ISlSApmWQxfKTUJqPP3-Kg6NU1Q"
+      #         }
+      #       ]
+      #     }
+      # @raise [JWT::EncodeError]
+      def generate_jwt(payload, private_keychain, algorithms)
         proxy_exception JWT::EncodeError do
-          { payload:    payload,
-            signatures: keychain.map { |id, value| generate_jws(payload, id, value, algorithms.fetch(id)) } }
+          { payload:    base64_encode(payload.to_json),
+            signatures: private_keychain.map { |id, value| generate_jws(payload, id, value, algorithms.fetch(id)) } }
         end
       end
 
+      #
+      # Verifies JWT.
+      #
+      # @param jwt [Hash]
+      #   The JWT in the format as defined in RFC 7515.
+      #   Example:
+      #     { "payload" => "eyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzODAsDQogImh0dHA6Ly9leGFtcGxlLmNvbS9pc19yb290Ijp0cnVlfQ",
+      #       "signatures" => [
+      #         { "protected" => "eyJhbGciOiJSUzI1NiJ9",
+      #           "header" => { "kid" => "2010-12-29" },
+      #           "signature" => "cC4hiUPoj9Eetdgtv3hF80EGrhuB__dzERat0XF9g2VtQgr9PJbu3XOiZj5RZmh7AAuHIm4Bh-0Qc_lF5YKt_O8W2Fp5jujGbds9uJdbF9CUAr7t1dnZcAcQjbKBYNX4BAynRFdiuB--f_nZLgrnbyTyWzO75vRK5h6xBArLIARNPvkSjtQBMHlb1L07Qe7K0GarZRmB_eSN9383LcOLn6_dO--xi12jzDwusC-eOkHWEsqtFZESc6BfI7noOPqvhJ1phCnvWh6IeYI2w9QOYEUipUTI8np6LbgGY9Fs98rqVt5AXLIhWkWywlVmtVrBp0igcN_IoypGlUPQGe77Rw"
+      #         },
+      #         { "protected" => "eyJhbGciOiJFUzI1NiJ9",
+      #           "header" => { "kid" => "e9bc097a-ce51-4036-9562-d2ade882db0d" },
+      #           "signature" => "DtEhU3ljbEg8L38VWAfUAqOyKAM6-Xx-F4GawxaepmXFCgfTjDxw5djxLa8ISlSApmWQxfKTUJqPP3-Kg6NU1Q"
+      #         }
+      #       ]
+      #     }
+      # @param public_keychain [Hash]
+      #   The hash which consists of pairs: key ID => public key.
+      #   The key may be presented as string in PEM format or as instance of {OpenSSL::PKey::PKey}
+      # @param options [Hash]
+      #   The rules for verifying JWT. The variable «algorithms» is always overwritten by the value from JWS header.
+      # @return [Hash]
+      #   Returns payload if all signatures are valid.
+      # @raise [JWT::DecodeError]
       def verify_jwt(jwt, public_keychain, options = {})
         proxy_exception JWT::DecodeError do
           jwt.fetch("signatures").each do |jws|
@@ -26,13 +77,26 @@ module JWT
         end
       end
 
-      # {
-      #   "protected":"eyJhbGciOiJFUzI1NiJ9",
-      #   "header":{
-      #     "kid":"e9bc097a-ce51-4036-9562-d2ade882db0d"
-      #   },
-      #   "signature":"DtEhU3ljbEg8L38VWAfUAqOyKAM6-Xx-F4GawxaepmXFCgfTjDxw5djxLa8ISlSApmWQxfKTUJqPP3-Kg6NU1Q"
-      # }
+      #
+      # Generates new JWS based on payload, key, and algorithm.
+      #
+      # @param payload [Hash]
+      # @param key_id [String]
+      #   The value which is used as «kid» in JWS header.
+      # @param key_value [String, OpenSSL::PKey::PKey]
+      #   The private key.
+      # @param algorithm [String]
+      #   The signature algorithm.
+      # @return [Hash]
+      #   The JWS in the format as defined in RFC 7515.
+      #   Example:
+      #     { protected: "eyJhbGciOiJFUzI1NiJ9",
+      #       header: {
+      #         kid: "e9bc097a-ce51-4036-9562-d2ade882db0d"
+      #       },
+      #       signature: "DtEhU3ljbEg8L38VWAfUAqOyKAM6-Xx-F4GawxaepmXFCgfTjDxw5djxLa8ISlSApmWQxfKTUJqPP3-Kg6NU1Q"
+      #     }
+      # @raise [JWT::EncodeError]
       def generate_jws(payload, key_id, key_value, algorithm)
         proxy_exception JWT::EncodeError do
           protected, _, signature = JWT.encode(payload, prepare(key_value, algorithm), algorithm).split(".")
@@ -42,6 +106,27 @@ module JWT
         end
       end
 
+      #
+      # Verifies JWS.
+      #
+      # @param jws [Hash]
+      #   The JWS in the format as defined in RFC 7515.
+      #   Example:
+      #     { "protected" => "eyJhbGciOiJFUzI1NiJ9",
+      #       "header" => {
+      #         "kid" => "e9bc097a-ce51-4036-9562-d2ade882db0d"
+      #       },
+      #       "signature" => "DtEhU3ljbEg8L38VWAfUAqOyKAM6-Xx-F4GawxaepmXFCgfTjDxw5djxLa8ISlSApmWQxfKTUJqPP3-Kg6NU1Q"
+      #     }
+      # @param payload [Hash]
+      # @param public_keychain [Hash]
+      #   The hash which consists of pairs: key ID => public key.
+      #   The key may be presented as string in PEM format or as instance of {OpenSSL::PKey::PKey}
+      # @param options [Hash]
+      #   The rules for verifying JWT. The variable «algorithms» is always overwritten by the value from JWS header.
+      # @return [Hash]
+      #   Returns payload if signature is valid.
+      # @raise [JWT::DecodeError]
       def verify_jws(jws, payload, public_keychain, options = {})
         proxy_exception JWT::DecodeError do
           encoded_header     = jws.fetch("protected")
