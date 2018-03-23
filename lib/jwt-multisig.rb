382067f4da48b45ff6c0904d2d6bca0313c56cda
Yaroslav Konoplov <eahome00@gmail.com>
2018-03-23 20:39:43 +0200

    Update public API

--- lib/jwt-multisig.rb
+++ lib/jwt-multisig.rb
@@ -7,18 +7,20 @@ require "openssl"
 module JWT
   module Multisig
     class << self
-      def encode(payload, keychain, algorithms)
-        { payload:    payload,
-          signatures: keychain.map { |id, value| generate_jws(payload, id, value, algorithms.fetch(id)) } }
-      rescue => e
-        JWT::EncodeError === e ? raise(e) : raise(JWT::EncodeError, e.inspect)
+      def generate_jwt(payload, keychain, algorithms)
+        proxy_exception JWT::EncodeError do
+          { payload:    payload,
+            signatures: keychain.map { |id, value| generate_jws(payload, id, value, algorithms.fetch(id)) } }
+        end
       end
 
-      def decode(jwt, public_keychain, token_verification_options = {})
-        jwt.fetch('signatures').each do |jws|
-          verify_jws!(jws, public_keychain, jws.fetch('payload'), token_verification_options)
+      def verify_jwt(jwt, public_keychain, options = {})
+        proxy_exception JWT::DecodeError do
+          jwt.fetch('signatures').each do |jws|
+            verify_jws(jws, public_keychain, jws.fetch('payload'), options)
+          end
+          JSON.parse(jwt.fetch('payload'))
         end
-        JSON.parse(jwt.fetch('payload'))
       end
 
       # {
@@ -29,25 +31,37 @@ module JWT
       #   "signature":"DtEhU3ljbEg8L38VWAfUAqOyKAM6-Xx-F4GawxaepmXFCgfTjDxw5djxLa8ISlSApmWQxfKTUJqPP3-Kg6NU1Q"
       # }
       def generate_jws(payload, key_id, key_value, algorithm)
-        key = if algorithm.start_with?('HS')
-          OpenSSL::PKey::PKey === key_value ? key_value.to_pem : key_value
-        else
-          OpenSSL::PKey::PKey === key_value ? key_value : OpenSSL::PKey.read(key_value)
+        proxy_exception JWT::EncodeError do
+          key = if algorithm.start_with?('HS')
+            OpenSSL::PKey::PKey === key_value ? key_value.to_pem : key_value
+          else
+            OpenSSL::PKey::PKey === key_value ? key_value : OpenSSL::PKey.read(key_value)
+          end
+  
+          protected, _, signature = JWT.encode(payload, key, algorithm).split(".")
+          { protected: protected,
+            header:    { kid: key_id },
+            signature: signature }
         end
-
-        protected, _, signature = JWT.encode(payload, key, algorithm).split(".")
-        { protected: protected,
-          header:    { kid: key_id },
-          signature: signature }
       end
 
-      def verify_jws!(jws_hash, public_keychain, encoded_payload, token_verification_options)
-        header     = jws_hash.fetch('protected')
-        payload    = encoded_payload
-        signature  = jws_hash.fetch('signature')
-        public_key = public_keychain.fetch(jws_hash.fetch('header').fetch('kid'))
-        jwt        = [header, payload, signature]
-        JWT.decode(jwt, public_key, true, token_verification_options.merge(algorithms: JSON.load(header).fetch('alg')))
+      def verify_jws(jws, public_keychain, payload, options = {})
+        proxy_exception JWT::DecodeError do
+          serialized_header  = jws.fetch('protected')
+          serialized_payload = payload.to_json
+          signature          = jws.fetch('signature')
+          public_key         = public_keychain.fetch(jws.fetch('header').fetch('kid'))
+          jwt                = [serialized_header, serialized_payload, signature]
+          JWT.decode(jwt, public_key, true, options.merge(algorithms: JSON.load(serialized_header).fetch('alg')))
+        end
+      end
+
+    private
+
+      def proxy_exception(exception_class)
+        yield
+      rescue => e
+        exception_class === e ? raise(e) : raise(exception_class, e.inspect)
       end
     end
   end
